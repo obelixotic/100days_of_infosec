c85eb2f46fdbcf57ce3ebdee2309f4062ee04a9c
asd0999 <obelixotic@gmail.com>
2020-03-20 13:41:41 -0400

    cleanup

--- program.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-#include"test.S"
-
-extern void test();
-
-int main() {
-    printf("Flag: 0x%x\n", test(0xaeed09cb,0xb7acde91,0xb7facecd));
-}
--- tar.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for ((i=1000; i>1; i--));
-do
-tar -xvf "${i}.tar"
-rm ${i}.tar
-done
